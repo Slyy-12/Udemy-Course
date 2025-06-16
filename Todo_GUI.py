6d6c2575b322b4d5d34ef8416eed329e798fbaf4
Karan <karanveer4196@gmail.com>
2025-06-16 22:24:31 +1000

    16/06

--- Todo_GUI.py
+++ Todo_GUI.py
@@ -7,12 +7,16 @@ input_box = sg.InputText(tooltip="Enter todo",
                          key="todo")
 add_button = sg.Button("Add")
 
+list_box = sg.Listbox(values=functions.get_todos(),
+                      key='todos', enable_events=True, size=[45,10])
+
 edit_button = sg.Button("Edit")
+
 complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window('My Todo-App', 
-                   layout=[[[label], [input_box, add_button]]], 
+                   layout=[[label], [input_box, add_button],[]], 
                    font=('Helvetica', 20))
 
 while True:
